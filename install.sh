ee5e3f8d32250954363503cdb729136fdf2ee0cc
vincyxiroff <88449249+vincyxiroff@users.noreply.github.com>
2023-02-14 00:06:21 +0100

    .

---
+++ install.sh
@@ -0,0 +1,1 @@
+apt update -y && apt install screenfetch git wondershaper snapd -y && snapd install lxd && lxd init && apt install python3 -y && apt Install Python3-pip -y && pip install discord psutil
