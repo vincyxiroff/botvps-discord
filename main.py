35dad0faca95460f05cd09f86944b3a355060d57
vincyxiroff <88449249+vincyxiroff@users.noreply.github.com>
2023-02-14 14:58:39 +0100

    Update main.py
    Not usable

--- main.py
+++ main.py
@@ -250,7 +250,7 @@ Panel: http://panel.vpsbot.ml:8888/""")
 
         
         os.system("rm .ssh/known_hosts") # Remove known_hosts file to prevent Bad Host Key error from paramiko in web panel
-        os.system(f"lxc config device override web-server {idprefix}{ctx.author.id}")
+        os.system(f"lxc config device override {idprefix}{ctx.author.id} eth0")
         os.system(f"lxc config device override web-server {idprefix}{ctx.author.id}")
         os.system(f"lxc exec {idprefix}{ctx.author.id} -- apt update -y")
         os.system(f"lxc exec {idprefix}{ctx.author.id} -- apt install openssh-server wget -y")
