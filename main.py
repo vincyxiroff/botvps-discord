35d3f3f26aa8601deda8e34655f76ee3684cbf2c
vincyxiroff <88449249+vincyxiroff@users.noreply.github.com>
2023-02-14 15:06:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# change 83.150.217.111 to ur ip
 import discord
 import os
 import sys
@@ -252,6 +253,7 @@ Panel: http://panel.vpsbot.ml:8888/""")
         os.system("rm .ssh/known_hosts") # Remove known_hosts file to prevent Bad Host Key error from paramiko in web panel
         os.system(f"lxc config device override {idprefix}{ctx.author.id} eth0")
         os.system(f"lxc config device set {idprefix}{ctx.author.id} eth0 ipv4.address 83.150.217.111")
+        os.system(f"lxc restart {idprefix}{ctx.author.id}")
         os.system(f"lxc exec {idprefix}{ctx.author.id} -- apt update -y")
         os.system(f"lxc exec {idprefix}{ctx.author.id} -- apt install openssh-server wget -y")
         os.system(f"lxc exec {idprefix}{ctx.author.id} -- rm /etc/ssh/sshd_config") # Remove known_hosts file to prevent Bad Host Key error from paramiko in web panel
